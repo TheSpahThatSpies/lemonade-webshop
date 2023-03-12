e9d3ac1772c37c307059dc4e9c632592db34c4cd
TheSpahThatSpies <christianborggaard@gmail.com>
2023-03-12 15:36:20 +0100

    Update shop-context.jsx

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from 'react'
+import { Product } from '../pages/shop/product';
 import { productList } from '../products';
 
 export const ShopContext = createContext(null);
@@ -15,16 +16,34 @@ const getDefaultCart = () => {
 export const ShopContextProvider = (props) => {
   const [CartProducts, setCartProducts] = useState(getDefaultCart());
 
+  // Calculate total price.
+  const getTotalAmount = () => {
+    let totalAmount = 0;
+    for (const item in CartProducts) {
+        if (CartProducts[item] > 0) {
+            let productInfo = productList.find((product) => product.id === Number(item));
+            totalAmount += CartProducts[item] * productInfo.price
+        }
+    }
+
+    return totalAmount;
+  }
+
   const addToCart = (productId) => {
     // Collect product ID and add 1.
-    setCartProducts((prev) => ({...prev, [productId]: prev[productId] + 1}))
+    setCartProducts((prev) => ({...prev, [productId]: prev[productId] + 1}));
   };
 
   const deleteFromCart = (productId) => {
     // Collect product ID and remove 1.
-    setCartProducts((prev) => ({...prev, [productId]: prev[productId] - 1}))
+    setCartProducts((prev) => ({...prev, [productId]: prev[productId] - 1}));
   };
 
-  const contextValue = {CartProducts, addToCart, deleteFromCart}
+  const updateCartProductCount = (newAmount, productId) => {
+    // Update product to manual input.
+    setCartProducts((prev) => ({...prev, [productId]: newAmount}));
+  };
+
+  const contextValue = {CartProducts, addToCart, deleteFromCart, updateCartProductCount, getTotalAmount };
   return (<ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>)
 }
