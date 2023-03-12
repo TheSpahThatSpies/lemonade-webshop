4bea5ab0464f7562cdfd91bad1654dbac0a145ab
TheSpahThatSpies <christianborggaard@gmail.com>
2023-03-12 15:36:16 +0100

    Update cart.jsx

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -2,10 +2,13 @@ import React, { useContext } from 'react';
 import { productList } from '../../products';
 import { ShopContext } from '../../context/shop-context';
 import { CartItem } from './cart-item';
+import { useNavigate } from 'react-router-dom';
 
 export const Cart = () => {
-  const {CartProducts} = useContext(ShopContext);
+  const {CartProducts, getTotalAmount} = useContext(ShopContext);
+  const totalAmount = getTotalAmount()
 
+  const navigate = useNavigate()
   return (
     <div className="cart">
         <div>
@@ -17,6 +20,12 @@ export const Cart = () => {
               return <CartItem data={product} />;
             }
         })}
+        </div>
+
+        <div className='checkout'>
+          <p> Total: {totalAmount} Kr.</p>
+          <button onClick={() => navigate("/")}> Continue Shopping</button>
+          <button> Checkout</button>
         </div>
     </div>
   )
